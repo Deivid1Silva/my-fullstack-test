d24cad506311c74037c42eff13017286fe5c3467
Deivid1Silva <deividcraft4004@gmail.com>
2026-03-10 20:05:16 -0500

    fix: add trusted origins to auth config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Toaster } from "sonner"; // Importamos el componente de notificaciones
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      {/* Toaster es el contenedor que "escucha" los comandos toast.success() 
+          y los muestra físicamente en la pantalla.
+      */}
+      <Toaster position="top-right" richColors closeButton />
+    </>
+  );
 }
